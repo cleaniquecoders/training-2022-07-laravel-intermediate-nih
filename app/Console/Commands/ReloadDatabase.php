0b3a9abe21064da0b2b832ec460f581e62e74f71
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-07-17 16:16:16 +0800

    Added reload-db command

---
+++ app/Console/Commands/ReloadDatabase.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ReloadDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reload-db {--dev} {--demo}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Flush database and seed';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->callSilently('migrate:fresh', ['--seed' => true]);
+
+        if($this->option('dev')) {
+            // seed dev seeder
+            $this->info('Successfully seed development data into the database.');
+        }
+
+        if($this->option('demo')) {
+            // seed demo seeder
+            $this->info('Successfully seed demo data into the database.');
+        }
+
+        $this->info('Successfully flush and seed the database.');
+        return 0;
+    }
+}
