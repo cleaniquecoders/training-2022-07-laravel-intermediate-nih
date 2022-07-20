d0ce88b691bba38ef539002e4185919a9dd1f6e3
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-07-20 15:13:51 +0800

    Added API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('users/{id}', function($id) {
+    return new UserResource(User::findOrFail($id));
+    // {"data":{"email":"murphy.meggie@example.net","name":"Alysha Sanford"}}
+});
+
+Route::get('users', function() {
+    return UserResource::collection(User::paginate(5));
 });
