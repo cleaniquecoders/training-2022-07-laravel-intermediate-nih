ca9546ab94b57225fbe91f64be4b39dad81ec085
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-07-20 11:19:05 +0800

    Added Validation Livewire

--- app/Http/Livewire/Feedback.php
+++ app/Http/Livewire/Feedback.php
@@ -6,6 +6,28 @@ use Livewire\Component;
 
 class Feedback extends Component
 {
+
+    public $name = '';
+    public $email = '';
+    public $feedback = '';
+    
+    public $rules = [
+        'name' => 'string|min:3|max:255|required',
+        'email' => 'email|min:3|max:255|required',
+        'feedback' => 'string|min:3|max:255|required',
+    ];
+
+    public function save()
+    {
+        $this->validate();
+
+        /// Feedback::create()....
+
+        session()->flash('message', 'Thank you for your feedback');
+
+        return redirect('/feedbacks');
+    }
+
     public function render()
     {
         return view('livewire.feedback');
--- resources/views/livewire/feedback.blade.php
+++ resources/views/livewire/feedback.blade.php
@@ -1,20 +1,37 @@
 <div>
-    <form action="POST">
+    @if(session()->has('message'))
+        <div class="flex justify-end">
+            <span class="text-black p-2 bg-green-200 rounded-md">{{ session('message') }}</span>
+        </div>
+    @endif
+    <form wire:submit.prevent="save">
         @csrf
 
         <div>
             <x-jet-label for="name" value="{{ __('Name') }}" />
-            <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" 
+                wire:model="name" required autofocus autocomplete="name" />
+            @error('name')
+                <span class="text-red-500">{{ $message }}</span>
+            @enderror
         </div>
 
         <div class="mt-4">
             <x-jet-label for="email" value="{{ __('Email') }}" />
-            <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+            <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" 
+                wire:model="email" required />
+            @error('email')
+                <span class="text-red-500">{{ $message }}</span>
+            @enderror
         </div>
 
         <div class="mt-4">
             <x-jet-label for="feedback" value="{{ __('Feedback') }}" />
-            <x-jet-input id="feedback" class="block mt-1 w-full" type="text" name="feedback" :value="old('feedback')" required />
+            <x-jet-input id="feedback" class="block mt-1 w-full" type="text" name="feedback" 
+                wire:model="feedback" required />
+            @error('feedback')
+                <span class="text-red-500">{{ $message }}</span>
+            @enderror
         </div>
 
         <div class="flex items-center justify-end mt-4">
